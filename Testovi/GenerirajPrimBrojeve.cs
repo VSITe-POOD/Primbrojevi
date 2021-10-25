e555037fceb4e6ac69917bdb71137213442ba9c4
Daniel Tošić <92323780+etosdan@users.noreply.github.com>
2021-10-25 18:31:48 +0200

    Add unit tests

---
+++ Testovi/GenerirajPrimBrojeve.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Vsite.Pood
+{
+    [TestClass]
+    public class TestGenerirajPrimBrojeve
+    {
+        [TestMethod]
+        public void ZaArgument0MetodaVraćaNizDuljine0()
+        {
+            var niz = Program.GenerirajPrimBrojeve(0);
+            Assert.AreEqual(0, niz.Length);
+        }
+        
+        [TestMethod]
+        public void ZaArgument1MetodaVraćaNizDuljine0()
+        {
+            var niz = Program.GenerirajPrimBrojeve(1);
+            Assert.AreEqual(0, niz.Length);
+        }
+        [TestMethod]
+        public void ZaArgument2MetodaVraćaNizDuljine1()
+        {
+            var niz = Program.GenerirajPrimBrojeve(2);
+            Assert.AreEqual(1, niz.Length);
+        }        
+        [TestMethod]
+        public void ZaArgument2MetodaVraćaNizSaPrvimElementom2()
+        {
+            var niz = Program.GenerirajPrimBrojeve(2);
+            Assert.AreEqual(2, niz[0]);
+        }        
+        [TestMethod]
+        public void ZaArgument100MetodaVraćaNizDuljine25()
+        {
+            var niz = Program.GenerirajPrimBrojeve(100);
+            Assert.AreEqual(25, niz.Length);
+        }        
+        [TestMethod]
+        public void ZaArgument100MetodaVraćaNizSaZadnjimElementom97()
+        {
+            var niz = Program.GenerirajPrimBrojeve(100);
+            Assert.AreEqual(97, niz.Last());
+        }
+    }
+}
